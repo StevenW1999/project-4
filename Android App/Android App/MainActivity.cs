cabc06e77eb689c75b5666f1327bb41afd8c8525
robinskil <33688350+robinskil@users.noreply.github.com>
2018-04-24 09:05:41 +0200

    Login page + Working code

--- Android App/Android App/MainActivity.cs
+++ Android App/Android App/MainActivity.cs
@@ -1,26 +1,41 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-
+using System.Diagnostics;
 namespace Android_App
 {
-    // "Theme = "@android:style/Theme.NoTitleBar"" added to remove title bar on top of application
-    [Activity(Label = "Android_App", MainLauncher = true, Icon = "@mipmap/icon" , Theme = "@style/Theme.Custom")]
+    // Theme = "style/Theme.Name" to load custom theme.
+    [Activity(Label = "Android_App", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/Theme.Custom")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        /// <summary>
+        /// This function is called upon starting the program => inheritance
+        /// </summary>
+        /// <param name="savedInstanceState"></param>
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
+            // Set our view from the "main" layout resource and getting the information from this field.
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            //Button button = FindViewById<Button>(Resource.Id.myButton);
+            // Get our button from the layout resource,and attach an event to it
+            Button loginButton = FindViewById<Button>(Resource.Id.LoginButton);
+            //Added a function to the click action of a the button
+            loginButton.Click += delegate { loginAction(); };
+        }
 
-            //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        /// <summary>
+        /// This function is called after the login button is clicked
+        /// Contains getting information from predefined fields in the AXML file.
+        /// </summary>
+        private void loginAction()
+        {
+            EditText usernameField = FindViewById<EditText>(Resource.Id.UsernameField);
+            string username = usernameField.Text;
+            EditText passwordField = FindViewById<EditText>(Resource.Id.PasswordField);
+            string password = passwordField.Text;
+            System.Diagnostics.Debug.WriteLine($"Given Username : {username} and password : {password}");
         }
     }
 }
