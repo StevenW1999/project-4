f322056002d21f231e53bbcb5aa29ce65a9d8c89
StevenW1999 <0932549@hr.nl>
2018-05-17 15:04:35 +0200

    Added Better Button

--- ASS/app/src/main/java/net/azurewebsites/ashittyscheduler/ass/addToDo.java
+++ ASS/app/src/main/java/net/azurewebsites/ashittyscheduler/ass/addToDo.java
@@ -1,5 +1,28 @@
 package net.azurewebsites.ashittyscheduler.ass;
 
-public class addtodo {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class addtodo extends AppCompatActivity{
+    ArrayList<String> todoList;
+    ArrayAdapter<String>adapter;
+    EditText todoTitle;
+    EditText todoDesc;
+    Date todoDate;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addtodo);
+
+    }
 
 }
