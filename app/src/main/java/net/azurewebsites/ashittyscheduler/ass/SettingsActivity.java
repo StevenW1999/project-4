0b16204bb89a471fc810ae29c84fe0d678fb3e18
6bq <31695264+6bq@users.noreply.github.com>
2018-05-19 01:53:47 +0200

    ☺

--- app/src/main/java/net/azurewebsites/ashittyscheduler/ass/SettingsActivity.java
+++ app/src/main/java/net/azurewebsites/ashittyscheduler/ass/SettingsActivity.java
@@ -10,6 +10,8 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         setTitle("Settings");
+
+        // ☺
     }
 
 }
