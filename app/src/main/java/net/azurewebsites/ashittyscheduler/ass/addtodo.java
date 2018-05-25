b31814208e0da144b2a57d14defa4e3c7ff2c494
StevenW1999 <0932549@hr.nl>
2018-05-25 15:30:03 +0200

    added time picker and calendar

--- app/src/main/java/net/azurewebsites/ashittyscheduler/ass/addtodo.java
+++ app/src/main/java/net/azurewebsites/ashittyscheduler/ass/addtodo.java
@@ -1,5 +1,9 @@
 package net.azurewebsites.ashittyscheduler.ass;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,7 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -25,13 +31,23 @@ private int CalendarHour;
 private  int CalendarMinute;
 private String format;
 TextView DisplayTime;
+TextView repeattText;
+private String mRepeat;
+private String mRepeatNo;
+private String mRepeatType;
+private TextView datepickerdialogbutton;
+private TextView selecteddate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtodo);
+
+
+        //Set Time Picker
         AccesTime = (TextView) findViewById(R.id.timePlainText);
         DisplayTime = (TextView) findViewById(R.id.timePlainText);
+        repeattText = (TextView) findViewById(R.id.repeatText) ;
         AccesTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,13 +76,80 @@ TextView DisplayTime;
                 timePickerDialog.show();
 
                     }
-
-
-
-
         });
 
+
+        datepickerdialogbutton = (TextView) findViewById(R.id.date);
+        selecteddate = (TextView)findViewById(R.id.date);
+
+        datepickerdialogbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                DialogFragment dialogfragment = new DatePickerDialogClass();
+
+                dialogfragment.show(getFragmentManager(), "Date Picker Dialog");
+
+            }
+        });
     }
+
+    public static class DatePickerDialogClass extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datepickerdialog = new DatePickerDialog(getActivity(),
+                    AlertDialog.THEME_DEVICE_DEFAULT_DARK,this,year,month,day);
+
+            return datepickerdialog;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day){
+
+            TextView textview = (TextView)getActivity().findViewById(R.id.date);
+
+            textview.setText(day + ":" + (month+1) + ":" + year);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+//    public void onSwitchRepeat(View view) {
+//        Switch on = (Switch) findViewById(R.id.repeatSwitch);
+//        if () {
+//            mRepeat = "true";
+//            repeattText.setText("Every " + mRepeatNo + " " + mRepeatType + "(s)");
+//        } else {
+//            mRepeat = "false";
+//            repeattText.setText(R.id.repeatText);
+//        }
+//    }
+
+
+
+
+
+
+
+
+
+
+    //add todo to the listview
     public void addButtonClicked (View v){
         String messageText = ((EditText)findViewById(R.id.titleText)).getText().toString();
         if (messageText.equals("")){
