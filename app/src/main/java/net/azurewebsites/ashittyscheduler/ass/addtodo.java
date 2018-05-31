fd7a267cdca14a44a38b5a6775949d7e68d86e22
StevenW1999 <0932549@hr.nl>
2018-05-31 14:40:19 +0200

    Added option to select interval and repeat type

--- app/src/main/java/net/azurewebsites/ashittyscheduler/ass/addtodo.java
+++ app/src/main/java/net/azurewebsites/ashittyscheduler/ass/addtodo.java
@@ -1,19 +1,51 @@
 package net.azurewebsites.ashittyscheduler.ass;
 
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 
-import java.sql.Time;
-import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
-public class addtodo extends AppCompatActivity {
+public class addtodo extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+TimePickerDialog timePickerDialog;
+Calendar calendar;
+private TextView AccesTime;
+private int CalendarHour;
+private  int CalendarMinute;
+private String format;
+TextView DisplayTime;
+TextView repeattText;
+private String mRepeat;
+private String mRepeatNo;
+private String mRepeatType;
+private TextView datepickerdialogbutton;
+private TextView selecteddate;
+private TextView mDateText, mTimeText, mRepeatText, mRepeatNoText, mRepeatTypeText, mRepeatIntervalText;
+private Switch repeatSwitch, notificationSwitch;
+private TextView notificationText;
+private AlarmManager alarmManager;
+private PendingIntent alarmIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +53,218 @@ public class addtodo extends AppCompatActivity {
         setContentView(R.layout.activity_addtodo);
 
 
+        //Set Time Picker
+        AccesTime = (TextView) findViewById(R.id.timePlainText);
+        DisplayTime = (TextView) findViewById(R.id.timePlainText);
+        repeattText = (TextView) findViewById(R.id.repeatText) ;
+        mRepeatTypeText = (TextView)findViewById(R.id.repeatType);
+        mRepeatText = (TextView) findViewById(R.id.repeatType);
+        mRepeatNoText = (TextView) findViewById(R.id.repeatType);
+        repeatSwitch = (Switch) findViewById(R.id.repeatSwitch);
+
+        mRepeatIntervalText = (TextView) findViewById(R.id.repeatInterval);
+
+
+
+        repeatSwitch.setOnCheckedChangeListener(this);
+
+//Clock
+        AccesTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar = Calendar.getInstance();
+                CalendarHour= calendar.get(Calendar.HOUR_OF_DAY);
+                CalendarMinute = calendar.get(Calendar.MINUTE);
+                timePickerDialog = new TimePickerDialog(addtodo.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        if (hourOfDay == 0) {
+                            hourOfDay += 12;
+                            format = "AM";
+
+                        } else if (hourOfDay == 12) {
+                            format = "PM";
+                        } else if (hourOfDay > 12) {
+                            hourOfDay -= 12;
+                            format = "PM";
+                        } else {
+                            format = "AM";
+                        }
+                        DisplayTime.setText(hourOfDay + ":" + minute + format);
+                    }
+                },
+                        CalendarHour,CalendarMinute, false);
+                timePickerDialog.show();
+
+                    }
+        });
+
+
+
+
+        datepickerdialogbutton = (TextView) findViewById(R.id.date);
+        selecteddate = (TextView)findViewById(R.id.date);
+
+        datepickerdialogbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                DialogFragment dialogfragment = new DatePickerDialogClass();
+
+                dialogfragment.show(getFragmentManager(), "Date Picker Dialog");
+
+            }
+        });
     }
+
+    //Repeat Switch
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (repeatSwitch.isChecked()){
+            repeattText.setText("Repeat ON");
+            mRepeatText.setText(  "SELECT REPEAT TYPE");
+            mRepeatIntervalText.setText("Interval");
+            mRepeatTypeText.setEnabled(true);
+            mRepeatIntervalText.setEnabled(true);
+
+
+        }
+        else {
+            repeattText.setText("Repeat OFF");
+            mRepeatText.setText("Repeat OFF");
+            mRepeatIntervalText.setText("Repeat OFF");
+            mRepeatTypeText.setEnabled(false);
+            mRepeatIntervalText.setEnabled(false);
+
+
+
+
+        }
+
+    }
+
+
+//Calendar
+    public static class DatePickerDialogClass extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datepickerdialog = new DatePickerDialog(getActivity(),
+                    AlertDialog.THEME_DEVICE_DEFAULT_DARK,this,year,month,day);
+
+            return datepickerdialog;
+
+
+
+
+
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day){
+
+            TextView textview = (TextView)getActivity().findViewById(R.id.date);
+
+            textview.setText(day + ":" + (month+1) + ":" + year);
+        }
+    }
+
+
+
+
+
+
+
+
+
+//select repeat type
+    public void selectRepeatType(View v){
+        final String[] items = new String[3];
+        items[0] = "Day";
+        items[1] = "Week";
+        items[2] = "Month";
+
+        // Create List Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Type");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int item) {
+
+                mRepeatType = items[item];
+                mRepeatTypeText.setText(mRepeatType);
+                mRepeatText.setText("Every " + mRepeatNo + " " + mRepeatType + "(s)");
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+//input repeat interval
+    public void setRepeatNo(View v){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Enter Number");
+
+        // Create EditText box to input repeat number
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        alert.setView(input);
+        alert.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                        if (input.getText().toString().length() == 0) {
+                            mRepeatNo = Integer.toString(1);
+                            mRepeatNoText.setText(mRepeatNo);
+                            mRepeatText.setText("Every " + mRepeatNo + " " + mRepeatType + "(s)");
+                        }
+                        else {
+                            mRepeatNo = input.getText().toString().trim();
+                            mRepeatNoText.setText(mRepeatNo);
+                            mRepeatText.setText("Every " + mRepeatNo + " " + mRepeatType + "(s)");
+                        }
+                    }
+                });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // do nothing
+            }
+        });
+        alert.show();
+    }
+
+
+
+
+
+
+
+
+
+    //add todo to the listview
     public void addButtonClicked (View v){
         String messageText = ((EditText)findViewById(R.id.titleText)).getText().toString();
+        String dateText = ((TextView)findViewById(R.id.date)).getText().toString();
         if (messageText.equals("")){
 
         }
         else {
             Intent intent = new Intent();
             intent.putExtra(Intent_Constants.INTENT_MESSAGE_FIELD, messageText);
+            intent.putExtra(Intent_Constants.KEY_DATE, dateText);
             setResult(Intent_Constants.INTENT_RESULT_CODE,intent);
             finish();
         }
     }
+
 }
 
 
--- app/src/main/java/net/azurewebsites/ashittyscheduler/ass/edittodo.java
+++ app/src/main/java/net/azurewebsites/ashittyscheduler/ass/edittodo.java
@@ -1,30 +1,49 @@
 package net.azurewebsites.ashittyscheduler.ass;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class edittodo extends AppCompatActivity {
+import java.util.Calendar;
+
+public class edittodo extends addtodo {
     String messageText;
     int position;
+    String dateText;
+    String ChangedDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtodo);
+
         Intent intent = getIntent();
+        dateText = intent.getStringExtra(Intent_Constants.KEY_DATE);
         messageText = intent.getStringExtra(Intent_Constants.INTENT_MESSAGE_DATA);
         position = intent.getIntExtra(Intent_Constants.INTENT_ITEM_POSITION, -1);
         EditText messageData = (EditText) findViewById(R.id.titleText);
+        TextView dateData = (TextView) findViewById(R.id.date);
+
         messageData.setText(messageText);
     }
     public void addButtonClicked(View v ){
+
         String changedMessageText = ((EditText)findViewById(R.id.titleText)).getText().toString();
         Intent intent = new Intent();
         intent.putExtra(Intent_Constants.INTENT_CHANGED_MESSAGE, changedMessageText);
+
         intent.putExtra(Intent_Constants.INTENT_ITEM_POSITION, position);
         setResult(Intent_Constants.INTENT_RESULT_CODE_TWO, intent);
         finish();
     }
+
+
+
 }
