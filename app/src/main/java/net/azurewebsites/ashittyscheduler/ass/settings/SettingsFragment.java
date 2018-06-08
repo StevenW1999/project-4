bf5d99e68478b4155ddb629341f90a3ba5b94569
6bq <31695264+6bq@users.noreply.github.com>
2018-06-08 14:28:20 +0200

    ....................

--- app/src/main/java/net/azurewebsites/ashittyscheduler/ass/settings/SettingsFragment.java
+++ app/src/main/java/net/azurewebsites/ashittyscheduler/ass/settings/SettingsFragment.java
@@ -14,9 +14,12 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.support.annotation.Nullable;
+import android.text.Editable;
 import android.util.Log;
 import android.util.Pair;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -25,6 +28,7 @@ import net.azurewebsites.ashittyscheduler.ass.ApplicationConstants;
 import net.azurewebsites.ashittyscheduler.ass.LoginActivity;
 import net.azurewebsites.ashittyscheduler.ass.MainMenu;
 import net.azurewebsites.ashittyscheduler.ass.R;
+import net.azurewebsites.ashittyscheduler.ass.User;
 import net.azurewebsites.ashittyscheduler.ass.http.AsyncHttpListener;
 import net.azurewebsites.ashittyscheduler.ass.http.HttpMethod;
 import net.azurewebsites.ashittyscheduler.ass.http.HttpResponse;
@@ -51,7 +55,8 @@ public class SettingsFragment extends Fragment {
     public static class MyPreferenceFragment extends PreferenceFragment {
 
         private EditTextPreference editDisplayName, editDescription;
-        private EditTextPreference editUsername, editEmail, editPassword;
+        private EditTextPreference editUsername, editEmail;
+        private Preference editPassword;
         private SwitchPreference notificationPreference, locationServicesPreference;
 
         public MyPreferenceFragment() {
@@ -78,7 +83,7 @@ public class SettingsFragment extends Fragment {
             editDescription = (EditTextPreference)preferenceScreen.findPreference("edit_description");
             editUsername = (EditTextPreference)preferenceScreen.findPreference("edit_username");
             editEmail = (EditTextPreference)preferenceScreen.findPreference("edit_email");
-            editPassword = (EditTextPreference)preferenceScreen.findPreference("edit_password");
+            editPassword = preferenceScreen.findPreference("edit_password");
             notificationPreference = (SwitchPreference)preferenceScreen.findPreference("switch_notifications_todos");
             locationServicesPreference = (SwitchPreference)preferenceScreen.findPreference("switch_location_services");
 
@@ -106,10 +111,56 @@ public class SettingsFragment extends Fragment {
                 }
             });
 
-            editPassword.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            editPassword.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    updatePreference(preference, newValue.toString(), "https://ashittyscheduler.azurewebsites.net/api/settings/changepassword", true);
+                public boolean onPreferenceClick(Preference preference) {
+                    final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getPreferenceScreen().getContext());
+
+                    LayoutInflater inflater = getActivity().getLayoutInflater();
+                    final View v = inflater.inflate(R.layout.dialog_changepassword, null);  // this line
+                    alertDialogBuilder.setView(v);
+
+                    final AlertDialog changePasswordDialog = alertDialogBuilder.create();
+
+                    final EditText passwordField = v.findViewById(R.id.passwordField);
+                    final EditText passwordField2 = v.findViewById(R.id.passwordField2);
+
+                    Button btnCancel = v.findViewById(R.id.cancelButton);
+                    Button btnChange = v.findViewById(R.id.changeButton);
+
+                    btnChange.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            // passwords must have a length of at least 5
+                            if (passwordField.length() < 5) {
+                                passwordField.setError("Your new password must contain at least 5 characters.");
+                                return;
+                            }
+
+                            // passwords must match
+                            if (!passwordField.getText().toString().equals(passwordField2.getText().toString())) {
+                                passwordField2.setError("Passwords do not match.");
+                                return;
+                            }
+
+                            updatePreference(editPassword, passwordField.getText().toString(),
+                                    "https://ashittyscheduler.azurewebsites.net/api/settings/changepassword",
+                                    false);
+
+                            changePasswordDialog.cancel();
+                        }
+                    });
+
+                    btnCancel.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            changePasswordDialog.cancel();
+                        }
+                    });
+
+                    changePasswordDialog.show();
+
                     return true;
                 }
             });
@@ -128,6 +179,7 @@ public class SettingsFragment extends Fragment {
 
         public void loadSettings(){
 
+            // Load the existing user settings into the fields
             HttpTask task = new HttpTask(this.getContext(),
                     HttpMethod.GET,
                     "http://ashittyscheduler.azurewebsites.net/api/users/self",
@@ -156,15 +208,19 @@ public class SettingsFragment extends Fragment {
 
                                     editDisplayName.setText(displayName);
                                     editDisplayName.setSummary(displayName);
+                                    editDisplayName.setDefaultValue(displayName);
 
                                     editUsername.setText(username);
                                     editUsername.setSummary(username);
+                                    editUsername.setDefaultValue(username);
 
                                     editEmail.setText(email);
                                     editEmail.setSummary(email);
+                                    editEmail.setDefaultValue(email);
 
                                     editDescription.setText(description);
                                     editDescription.setSummary(description);
+                                    editDescription.setDefaultValue(description);
 
                                 } catch (JSONException e) {
                                     e.printStackTrace();
@@ -186,6 +242,9 @@ public class SettingsFragment extends Fragment {
             task.execute();
         }
 
+        /*
+        Updates a single preference and optionally updates its summary with the given value
+         */
         public void updatePreference(final Preference preference, final String value, final String url, final boolean updateSummary) {
             // Body parameters
             Pair[] parameters = new Pair[] {
@@ -201,7 +260,7 @@ public class SettingsFragment extends Fragment {
 
                         @Override
                         public void onBeforeExecute() {
-                            // show a progress dialog? disabled
+                            // show a progress dialog? currently disabled
                             //progressDialog = ProgressDialog.show(getPreferenceScreen().getContext(),"Updating","Please wait");
                         }
 
@@ -220,6 +279,7 @@ public class SettingsFragment extends Fragment {
 
                         @Override
                         public void onError() {
+                            // Show an error dialog
                             new AlertDialog.Builder(getPreferenceScreen().getContext())
                                     .setTitle("Error")
                                     .setMessage("Something went wrong. Please try again later.")
